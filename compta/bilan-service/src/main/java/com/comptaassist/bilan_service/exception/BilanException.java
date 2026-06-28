358ebfd9d131bfe0af78573999d4de10b96a095f
Yasmine BENOMAR <yasmine.benomar@teknologiate.com>
2026-06-28 12:40:25 +0200

    Suppression du service inutilisé

--- compta/bilan-service/src/main/java/com/comptaassist/bilan_service/exception/BilanException.java
+++
@@ -1,8 +0,0 @@
-// BilanException.java
-package com.comptaassist.bilan_service.exception;
-
-public class BilanException extends RuntimeException {
-    public BilanException(String message) {
-        super(message);
-    }
-}
